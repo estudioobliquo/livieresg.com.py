2bd3e60ef52f3d96ec67b77fdb530b6d649222cc
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2023-07-03 17:46:10 -0400

    Added: eslint settings and files

---
+++ stylelint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard-scss', // configure for SCSS
+    'stylelint-config-recommended-vue', // add overrides for .Vue files
+    'stylelint-config-recess-order', // use the recess order for properties
+  ],
+  rules: {
+    'no-descending-specificity': null,
+  },
+}
