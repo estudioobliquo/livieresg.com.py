a179a994edc9ee503b1f114d29bb66584bed3845
Lucas Boh <l.boh@onveda.de>
2023-07-06 18:59:55 +0200

    Added: single post page

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
+  <div class="blog-page">
     <h1>Blog</h1>
     <div v-if="posts" class="posts-container">
-      <div v-for="post in posts" :key="post.slug" class="post">
+      <NuxtLink v-for="post in posts" :key="post. slug" :to="`/blog/${post.slug}`" class="post">
         <h2>{{ post.title }}</h2>
         <p class="date">
           {{ post.date }}
@@ -10,22 +10,14 @@
         <p class="excerpt">
           {{ post.body }}
         </p>
-      </div>
+      </NuxtLink>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { TinaMarkdownContent } from 'tinacms/dist/rich-text'
 import { client } from '@/tina/__generated__/client'
-
-interface Post {
-  slug: string
-  title: string
-  type: string
-  date: string
-  body: TinaMarkdownContent
-}
+import { Post } from '~/tina/__generated__/types'
 
 // TODO: remove
 definePageMeta({ layout: 'central' })
