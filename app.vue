5809e9d454ebec266d2451bc9296b477e8a49684
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2023-07-19 14:14:39 -0400

    Deleted: slide transition

--- app.vue
+++ app.vue
@@ -43,17 +43,6 @@ if (!posts.value.length) {
 </script>
 
 <style>
-.slide-enter-active,
-.slide-leave-active {
-  transition: opacity 0.4s ease-in-out, transform 0.4s ease-in-out;
-}
-
-.slide-enter,
-.slide-leave-to {
-  opacity: 0;
-  transform: translateX(100%);
-}
-
 .opacity-enter-active,
 .opacity-leave-active {
   transition: opacity 0.4s ease-in-out;
