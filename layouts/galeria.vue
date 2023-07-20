fd8f447e830db9f0ee558cf9d09ee1b62917a641
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2023-07-20 12:06:54 -0400

    Fixed; menu mobile in gallery page

--- layouts/galeria.vue
+++ layouts/galeria.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <LayoutHeader />
+    <div class="mobileHeader">
+      <LayoutMenu :menu-active="showMobileMenu" @closeMobileMenu="showMobileMenu = false" />
+    </div>
+    <LayoutHeader @openMobileMenu="showMobileMenu = true" />
     <div class="galleryLayoutWrapper">
       <slot />
     </div>
@@ -8,11 +11,20 @@
   </div>
 </template>
 
+<script setup lang="ts">
+const showMobileMenu = ref(false)
+</script>
 <style>
 .galleryLayoutWrapper {
   width: 86%;
   max-width: 1340px;
   padding: 0;
   margin: 0 auto;
+}
+
+@media only screen and (min-width:1000px) {
+  .mobileHeader {
+    display: none;
+  }
 }
 </style>
